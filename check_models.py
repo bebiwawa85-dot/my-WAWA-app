fe9a21b995b4c47a34ea6dc2af174e23e2bbece3
bebiwawa85-dot <bebiwawa85@gmail.com>
2025-08-04 17:41:19 +0700

    Delete check_models.py

--- check_models.py
+++
@@ -1,5 +0,0 @@
-from together import Together
-client = Together(api_key="5cf3f4d760983464420305abfb35dd322be02f47bf5e86ac95910ceebc58712e")
-models = client.models.list()
-for m in models:
-    print(m)
